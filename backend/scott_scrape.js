74e4c52360f5e0ee5ee433759584db26f11dd355
Chad McKee <chadm_@yahoo.com>
2017-09-08 22:08:31 -0400

    scott request scrape

---
+++ backend/scott_scrape.js
@@ -0,0 +1,47 @@
+var casper = require('casper').create({
+    pageSettings: {
+        loadImages:  false,
+        loadPlugins: false
+    }
+});
+
+casper.start();
+
+casper.thenOpen('http://athensga.chambermaster.com/list', function () {
+    this.echo(this.getTitle());
+    if (this.exists('div.sidebar-wod')) {
+        //this.echo('success');
+        var wodLinks = this.getElementsAttribute('a.sidebar-readmore', 'href');
+
+        for(var i = 0; i < wodLinks.length; i++){
+
+            casper.thenOpen(wodLinks[i], function () {
+
+                if (this.exists('div.entry-content')) {
+                    var title = this.getTitle();
+                    var wod = this.getHTML('div.entry-content');
+
+                    console.log(title);
+                    console.log(wod);
+
+                    casper.thenOpen('http://wchadmckee.com:4050/setWODs', {
+
+                        method: 'POST',
+                        data: {
+                            'Title': title,
+                            'WOD': wod,
+                        }
+                    });
+
+                }
+
+            })
+        }
+    }
+    // else{
+    //    this.echo('failure')
+    // }
+
+});
+
+casper.run();
